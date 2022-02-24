1248a951061e3d4baaac6633f1548f6a3f43f488
juun9714 <lovekwon200@gmail.com>
2022-02-24 13:33:26 +0900

    comment added

--- assignment1.py
+++ assignment1.py
@@ -8,25 +8,36 @@ n_zero=0
 
 
 data=[(0,0),(1,1),(1,2),(2,1)]
+# gt : 에러함수를 각 가중치로 미분한 값 (근데 모든 합인 에러함수를 미분한게 아니라, 각 data에 따라 미분해서 합한 것임) 
+# => 미분한 일반식을 구해서 데이터를 넣어서 그걸 다 합친 것임
+# => 근데 미분한 일반식이 w에 따라서 달라짐 
+#t에 따라서 w가 계속 바뀌는데, gt도 t에 따른 w에 따라서 계속 x값을 대입해서 바뀌는 식임, w에 따라서 x값은 고정 
+# 고정된 x값은 data를 통해서 제공하고, zero, one, two는 계속 바뀜
+
+
 def Fzero(data, zero, one, two):
+    #gt0
+    #8w0
     sum=0
     for i in range(4):
-        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero)
+        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero) #-2*(y-f(x;w))
         
     return sum
 
 
 def Fone(data, zero, one, two):
+    #gt1
     sum=0
     for i in range(4):
-        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero)*data[i][0]
+        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero)*data[i][0] #-2*(y-f(x;w))*x
     
     return sum
 
 def Ftwo(data, zero, one, two):
+    #gt2
     sum=0
     for i in range(4):
-        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero)*(data[i][0]**2)
+        sum+=-2*(data[i][1]-(two*(data[i][0]**2)) - one*(data[i][0]) - zero)*(data[i][0]**2) #-2*(y-f(x;w))*x^2
     
     return sum
 
@@ -45,4 +56,3 @@ for i in range(0,100000):
     zero=n_zero
 
 print("after : ",n_two,"x^2 + ", n_one,"x + ", n_zero)
-
